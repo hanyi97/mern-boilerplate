e3775b34b1e3d020bc1d04d2af0e05139f178508
HanYi <nghanyi1997@gmail.com>
2021-11-02 09:04:59 +0800

    Bugfix for mongodb configs

--- server/config/db.js
+++ server/config/db.js
@@ -4,9 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useCreateIndex: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
     });
 
     console.log("MongoDB Connected...");
